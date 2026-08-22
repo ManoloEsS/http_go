a5c5a55764afa57e9a0434772b54238e25391b8f
ManoloEsS <manolo.estrada.s@gmail.com>
2026-08-22 17:25:06 -0600

    channel refactor

--- main.go
+++ main.go
@@ -16,35 +16,47 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
-	defer file.Close()
 
-	buf := make([]byte, 8, 8)
-	var line strings.Builder
+	linesChan := getLinesChannel(file)
+	for line := range linesChan {
+		fmt.Println("read:", line)
+	}
+}
 
-	for {
-		n, err := file.Read(buf)
-		if err != nil {
-			if line.Len() > 0 {
-				fmt.Printf("read: %s\n", line.String())
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		buf := make([]byte, 8, 8)
+		var line strings.Builder
+
+		for {
+			n, err := f.Read(buf)
+			if err != nil {
+				if line.Len() > 0 {
+					lines <- line.String()
+					line.Reset()
+				}
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+				return
+			}
+
+			str := string(buf[:n])
+			parts := strings.Split(str, "\n")
+
+			for i := 0; i < len(parts)-1; i++ {
+				line.WriteString(parts[i])
+				lines <- line.String()
 				line.Reset()
 			}
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			break
+
+			line.WriteString(parts[len(parts)-1])
+
 		}
-
-		str := string(buf[:n])
-		parts := strings.Split(str, "\n")
-
-		for i := 0; i < len(parts)-1; i++ {
-			line.WriteString(parts[i])
-			fmt.Printf("read: %s\n", line.String())
-			line.Reset()
-		}
-
-		line.WriteString(parts[len(parts)-1])
-
-	}
+	}()
+	return lines
 }
