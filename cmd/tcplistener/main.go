b45a1c38e7da0a4ede4d8a295fd09c0f1d7760a2
ManoloEsS <manolo.estrada.s@gmail.com>
2026-08-30 17:43:22 -0600

    add wiring for the tcplistener with the request from reader

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/ManoloEsS/http_go/internal/request"
 )
 
 const (
@@ -29,51 +28,56 @@ func main() {
 			fmt.Printf("could not start connection: %s\n", err.Error())
 		}
 
-		fmt.Println("Connection has been accepted from", conn.RemoteAddr())
+		fmt.Println("Connection has been accepted from", conn.RemoteAddr(), "\n")
 
-		linesChan := getLinesChannel(conn)
-		for line := range linesChan {
-			fmt.Println(line)
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("error parsing request: %s\n", err.Error())
 		}
+
+		fmt.Printf(
+			"Request line:\n- Method: %s\n- Target: %s\n- Version: %s\n\n",
+			req.RequestLine.Method, req.RequestLine.RequestTarget, req.RequestLine.HttpVersion,
+		)
 
 		fmt.Println("Connection has been closed to", conn.RemoteAddr())
 	}
 }
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-	go func() {
-		defer f.Close()
-		defer close(lines)
-		buf := make([]byte, 8, 8)
-		var line strings.Builder
-
-		for {
-			n, err := f.Read(buf)
-			if err != nil {
-				if line.Len() > 0 {
-					lines <- line.String()
-					line.Reset()
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				return
-			}
-
-			str := string(buf[:n])
-			parts := strings.Split(str, "\n")
-
-			for i := 0; i < len(parts)-1; i++ {
-				line.WriteString(parts[i])
-				lines <- line.String()
-				line.Reset()
-			}
-
-			line.WriteString(parts[len(parts)-1])
-
-		}
-	}()
-	return lines
-}
+// func getLinesChannel(f io.ReadCloser) <-chan string {
+// 	lines := make(chan string)
+// 	go func() {
+// 		defer f.Close()
+// 		defer close(lines)
+// 		buf := make([]byte, 8, 8)
+// 		var line strings.Builder
+//
+// 		for {
+// 			n, err := f.Read(buf)
+// 			if err != nil {
+// 				if line.Len() > 0 {
+// 					lines <- line.String()
+// 					line.Reset()
+// 				}
+// 				if errors.Is(err, io.EOF) {
+// 					break
+// 				}
+// 				fmt.Printf("error: %s\n", err.Error())
+// 				return
+// 			}
+//
+// 			str := string(buf[:n])
+// 			parts := strings.Split(str, "\n")
+//
+// 			for i := 0; i < len(parts)-1; i++ {
+// 				line.WriteString(parts[i])
+// 				lines <- line.String()
+// 				line.Reset()
+// 			}
+//
+// 			line.WriteString(parts[len(parts)-1])
+//
+// 		}
+// 	}()
+// 	return lines
+// }
